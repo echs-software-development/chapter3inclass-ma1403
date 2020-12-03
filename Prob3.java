cadf80bc0bc0b2219be82eb880d1043645d66315
ma1403 <73349536+ma1403@users.noreply.github.com>
2020-12-03 15:06:01 +0000

    problem 3

--- Prob3.java
+++ Prob3.java
@@ -2,7 +2,26 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
+    Scanner scan= new Scanner(System.in);
+    System.out.println("Please enter a between 0-4: ");
+
+    int grade=scan.nextInt();
+
+    if (grade ==4) 
+      System.out.println("A");
+
+    else if (grade ==3)
+      System.out.println("B");
     
+    else if (grade == 2)
+      System.out.println("C");
+    
+    else if (grade == 1)
+      System.out.println("D");
+    
+    else {
+      System.out.println("F");
+    }
 
 
     
--- Prob4.java
+++ Prob4.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
-    
-
+  Scanner scan= new Scanner(System.in);
+  
 
     
   }
